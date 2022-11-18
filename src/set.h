fc37c891e2f8dd0b632a9c127340ada8ae34a7bf
Badr El Habti <Badr.El_Habti@enseirb-matmeca.fr>
2022-11-18 16:02:59 +0100

    new_set_h

--- src/set.h
+++ src/set.h
@@ -1,10 +1,19 @@
 #ifndef __ENSEMBLE_H__
 #define __ENSEMBLE_H__
 
+#include <stdio.h>
+#include <stdlib.h>
 #include "geometry.h"
 
 
-struct set_t;
+typedef struct{
+unsigned int * ptr;
+ int size;
+} set_t;
+
+set_t init_set (int size);
+
+set_t resize_set (set_t ens, int new_size);
 
 
 #endif // __ENSEMBLE_H__
