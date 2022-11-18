909870a9d4c2f03b2bfcaf341d241ce2afa660b9
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2022-11-18 15:03:17 +0100

    geometry test

--- src/geometry.c
+++ src/geometry.c
@@ -2,22 +2,17 @@
 #include <stdlib.h>
 #include "geometry.h"
 
-const char* names_color[] = {
-  "NO_COLOR","BLACK","WHITE"
-};
-
 const char* names_sort[] = {
   "NO_SORT","PAWN"
 };
 
-//char result[32];
-
-static const char* place_strings[MAX_SORT][MAX_COLOR];
+static const char* place_strings[MAX_SORT][MAX_COLOR] = {  
+   {"NO_SORT NO_COLOR", "NO_SORT BLACK", "NO_SORT WHITE"} ,   
+   {"PAWN NO_COLOR", "PAWN BLACK", "PAWN WHITE"} ,   
+};
 
 const char* place_to_string(enum color_t c, enum sort_t s){
-  // sprintf(result,"%s %s", names_color[c], names_sort[s]);
-  static const char* result[MAX_SORT][MAX_COLOR] = {names_sort[c], names_color[s]} ;
-  return result;
+  return place_strings[s][c];
 }
 
 const char* names_dir[] = {
