41cb406273db9a6081ab5cdfe8bd006d5bc07da0
Badr El Habti <Badr.El_Habti@enseirb-matmeca.fr>
2022-11-22 18:17:34 +0100

    fixing_stuff

--- src/set.h
+++ src/set.h
@@ -18,22 +18,14 @@ set_t init_set(unsigned int size);
 /** Resize the set's size with realloc */
 void resize_set(set_t *set, int new_size);
 
-<<<<<<< HEAD
-void push_set(set_t *set, unsigned int x);
-=======
 /** Add an element at the end of the set */
-void append_set(set_t *set, unsigned int x);
->>>>>>> a1a1c1523d7a983e509580aa48fca62a71365e24
+void push_set(set_t *set, unsigned int x);
 
 /** Remove the last element and return it */
 unsigned int pop_set(set_t *set);
 
-<<<<<<< HEAD
-unsigned int exist_in_set(set_t *set, unsigned int x);
-=======
 /** Check if an element exists in the set and return his index */
-unsigned int exist_set(set_t *set, unsigned int x);
->>>>>>> a1a1c1523d7a983e509580aa48fca62a71365e24
+unsigned int exist_in_set(set_t *set, unsigned int x);
 
 /** Change the value of an element in the set */
 void modif_set(set_t *set, unsigned int x, unsigned int y);
