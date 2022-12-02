42e242a4c962b3e1928748212c4dd768f815ac1e
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2022-12-02 13:09:55 +0100

    add_prototypes_projecth

--- src/project.h
+++ src/project.h
@@ -11,4 +11,8 @@ int check_simple_victory(unsigned int idx, unsigned int p);
 
 int check_complex_victory(unsigned int p);
 
+unsigned int choose_random_move_for_piece(struct world_t *w, unsigned int p);
+
+void move_piece(struct world_t *w, unsigned int p, unsigned int m);
+
 #endif // __PROJECT_H__
