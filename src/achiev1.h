679e7d93957774e51abe547e067ce20f45f44abe
Badr El Habti <Badr.El_Habti@enseirb-matmeca.fr>
2022-12-09 01:10:37 +0100

    bug fix

--- src/achiev1.h
+++ src/achiev1.h
@@ -12,6 +12,6 @@ set_t possible_mvts_tower(unsigned int idx, struct world_t *w);
 
 /** COMMENT HERE */
 
-set_t possible_mvts_elephant(unsigned int idx, struct world_t *w)
+set_t possible_mvts_elephant(unsigned int idx, struct world_t *w);
 
 #endif // __ACHIEV1_H__
