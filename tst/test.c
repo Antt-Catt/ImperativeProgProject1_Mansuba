a57404a0e808e9dc6419eaf858c30a9ca29cb21b
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2022-12-30 15:59:20 +0100

    split_tests_in_functions

--- tst/test.c
+++ tst/test.c
@@ -4,23 +4,16 @@
 
 #include "test.h"
 
-int main(int argc, char *argv[])
+void test_geometry()
 {
-  argc = argc;
-  argv[0] = argv[0];
-  init_neighbors(0);
-
-  printf("\n");
-  // tests geometry
-  printf("geometry tests :\n");
   printf("North west direction : %s\n", dir_to_string(4));
   printf("A white pawn in this place : %s\n", place_to_string(2, 1));
   printf("A black elephant in this place : %s\n", place_to_string(1, 3));
   printf("A white tower in this place : %s\n", place_to_string(2, 2));
+}
 
-  printf("\n\n");
-  // tests world
-  printf("world tests :\n");
+void test_world()
+{
   printf("world init :\n");
   struct world_t *w = world_init();
   for (int i = 0; i < WORLD_SIZE; i++)
@@ -35,10 +28,10 @@ int main(int argc, char *argv[])
   printf("place 5, color %d, sort %d\n", world_get(w, 5), world_get_sort(w, 5));
   printf("set place 16 at color 2 and sort 1\n");
   printf("place 16, color %d, sort %d\n", world_get(w, 16), world_get_sort(w, 16));
+}
 
-  printf("\n\n");
-  // tests neighbors
-  printf("neighbors tests :\n");
+void test_neighbors()
+{
   printf("neighbor of %d in direction %s is %d ; expected -1\n", 0, dir_to_string(2), get_neighbor(0, 2));
   printf("neighbor of %d in direction %s is %d ; expected 5\n", 0, dir_to_string(-3), get_neighbor(0, -3));
   printf("neighbor of %d in direction %s is %d ; expected 6\n", 12, dir_to_string(4), get_neighbor(12, 4));
@@ -51,8 +44,7 @@ int main(int argc, char *argv[])
   {
     printf("%d ", result.n[p].i);
   }
-  printf("\nexpected 14 13 8 4 3 -1 0 0 0\n");
-  printf("\n");
+  printf("\nexpected 14 13 8 4 3 -1 0 0 0\n\n");
 
   result = get_neighbors(15);
   printf("neighbors of 15 in all directions are ");
@@ -60,41 +52,49 @@ int main(int argc, char *argv[])
   {
     printf("%d ", result.n[p].i);
   }
-  printf("\nexpected 16 11 10 -1 0 0 0 0 0\n");
-  printf("\n");
+  printf("\nexpected 16 11 10 -1 0 0 0 0 0\n\n");
 
-  printf("\n");
-  // tests set
-  printf("set tests :\n");
-  set_t ens = init_set(0);
-  push_set(&ens, 17);
-  print_set(&ens);
+  result = get_neighbors(7);
+  printf("neighbors of 7 in all directions are ");
+  for (int p = 0; p < MAX_NEIGHBORS + 1; p++)
+  {
+    printf("%d ", result.n[p].i);
+  }
+  printf("\nexpected 13 12 11 6 8 3 2 1 -1\n");
+}
+
+void test_set()
+{
+  set_t set = init_set(0);
+  push_set(&set, 17);
+  print_set(&set);
   printf("{ 17 } expected\n\n");
   // int x = ens.size;
-  modif_set(&ens, 17, 19);
-  print_set(&ens);
+  modif_set(&set, 17, 19);
+  print_set(&set);
   printf("{ 19 } expected\n\n");
-  push_set(&ens, 7);
-  push_set(&ens, 12);
-  push_set(&ens, 43);
-  push_set(&ens, 15);
-  print_set(&ens);
+  push_set(&set, 7);
+  push_set(&set, 12);
+  push_set(&set, 43);
+  push_set(&set, 15);
+  print_set(&set);
   printf("{ 19 7 12 43 15 } expected\n\n");
-  printf("set size = %d ; expected 5\n\n", ens.size);
-  printf("43 is in idx %d ; expected 3\n\n", exist_in_set(&ens, 43));
-  printf("last elt is %d ; expected 15\n\n", pop_set(&ens));
-  print_set(&ens);
+  printf("set size = %d ; expected 5\n\n", set.size);
+  printf("43 is in idx %d ; expected 3\n\n", exist_in_set(&set, 43));
+  printf("last elt is %d ; expected 15\n\n", pop_set(&set));
+  print_set(&set);
   printf("{ 19 7 12 43 } expected\n");
-  printf("set size = %d ; expected 4\n\n", ens.size);
-  delete_from_set(&ens, 12);
-  print_set(&ens);
+  printf("set size = %d ; expected 4\n\n", set.size);
+  delete_from_set(&set, 12);
+  print_set(&set);
   printf("{ 19 7 43 } expected\n");
-  printf("final set size = %d ; expected 3\n\n", ens.size);
+  printf("final set size = %d ; expected 3\n", set.size);
+  delete_set(&set);
+}
 
-  printf("\n");
-  // tests movements
-  printf("movements tests :\n");
-  w = world_init();
+void test_movements()
+{
+  struct world_t* w = world_init();
   world_set_sort(w, 6, 1);
   world_set(w, 6, 2);
   world_set_sort(w, 7, 1);
@@ -112,8 +112,21 @@ int main(int argc, char *argv[])
   printf("{ 1 8 18 16 5 } expected\n");
   move_piece(w, 6, set.ptr[2]);
   print_world(w);
+  delete_set(&set);
+}
 
-  // tests achiev2
+void test_game()
+{
+
+}
+
+void test_achiev1()
+{
+
+}
+
+void test_achiev2()
+{
   init_neighbors(2);
   printf("neighbor of %d in dir %s is %d ; expected 16\n", 0, dir_to_string(2), get_neighbor(0, 2));
   printf("neighbor of %d in direction %s is %d ; expected -1\n", 0, dir_to_string(4), get_neighbor(0, 4));
@@ -124,7 +137,7 @@ int main(int argc, char *argv[])
   printf("neighbor of %d in direction %s is %d ; expected 2\n", 16, dir_to_string(-4), get_neighbor(16, -4));
   printf("\n");
 
-  result = get_neighbors(9);
+  struct neighbors_t result = get_neighbors(9);
   printf("neighbors of 9 in all directions are ");
   for (int p = 0; p < MAX_NEIGHBORS + 1; p++)
   {
@@ -143,10 +156,13 @@ int main(int argc, char *argv[])
     printf("%d ", result.n[p].i);
   }
   printf("\nexpected 3 2 1 16 18 13 12 11 -1\n");
+}
 
-  // tests achiev3
-  extern unsigned int achiev3;
-  extern set_t black_prison;
+void test_achiev3()
+{
+  struct world_t * w = world_init();
+  /*extern unsigned int achiev3;
+    extern set_t black_prison;*/
   print_world(w);
   achiev3 = 1;
   move_piece(w, 11, 7);
@@ -156,5 +172,54 @@ int main(int argc, char *argv[])
   escape(BLACK, w);
   print_world(w);
   print_set(&black_prison);
+}
+
+int main(int argc, char *argv[])
+{
+  argc = argc;
+  argv[0] = argv[0];
+  init_neighbors(0);
+
+  achiev3 = 0;
+  achiev4 = 0;
+  init_neighbors(0);
+  
+  printf("\n");
+  // tests geometry
+  printf("geometry tests :\n");
+  test_geometry();
+  printf("\n\n");
+  
+  // tests world
+  printf("world tests :\n");
+  test_world();
+  printf("\n\n");
+  
+  // tests neighbors
+  printf("neighbors tests :\n");
+  test_neighbors();
+  printf("\n\n");
+  
+  // tests set
+  printf("set tests :\n");
+  test_set();
+  printf("\n\n");
+  
+  // tests movements
+  printf("movements tests :\n");
+  test_movements();
+  printf("\n\n");
+
+  // tests achiev2
+  printf("achiev 2 tests :\n");
+  test_achiev2();
+  printf("\n\n");
+  
+  // tests achiev3
+  printf("achiev3 tests : \n");
+  test_achiev3();
+  printf("\n\n");
+
+  
   return 0;
 }
