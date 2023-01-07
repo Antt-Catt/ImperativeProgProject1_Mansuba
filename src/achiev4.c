6ef70cfec733f302134f4f6bf8465573f182737b
Badr El Habti <Badr.El_Habti@enseirb-matmeca.fr>
2023-01-07 14:25:24 +0100

    x

---
+++ src/achiev4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "achiev4.h"
+
+set_t achiev4_mvts(set_t *set, unsigned int player)
+{
+  unsigned int tmp;
+  set_t result = init_set(0);
+  if (player == BLACK)
+  {
+    tmp = 0;
+    for (int i = 0; i < (*set).size; i++)
+    {
+      if ((*set).ptr[i] % WIDTH > tmp)
+      {
+        tmp = (*set).ptr[i] % WIDTH;
+      }
+    }
+    for (int i = 0; i < (*set).size; i++)
+    {
+      if ((*set).ptr[i] % WIDTH == tmp)
+      {
+        push_set(&result, (*set).ptr[i]);
+      }
+    }
+  }
+  else if (player == WHITE)
+  {
+    tmp = WIDTH;
+    for (int i = 0; i < (*set).size; i++)
+    {
+      if ((*set).ptr[i] % WIDTH < tmp)
+      {
+        tmp = (*set).ptr[i] % WIDTH;
+      }
+    }
+    for (int i = 0; i < (*set).size; i++)
+    {
+      if ((*set).ptr[i] % WIDTH == tmp)
+      {
+        push_set(&result, (*set).ptr[i]);
+      }
+    }
+  }
+  delete_set(set);
+  return result;
+}
---
+++ src/achiev4.h
@@ -0,0 +1,8 @@
+#ifndef __ACHIEV4_H__
+#define __ACHIEV4_H__
+
+#include "movements.h"
+
+set_t achiev4_mvts(set_t *set, unsigned int player);
+
+#endif // __ACHIEV4_H__
